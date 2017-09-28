b4e284396c7a5e039f056f64a7debfe3c42e298c
scalka <calka.sylwia@gmail.com>
2017-09-28 14:52:40 +0100

    basics

--- startingKit/js.js
+++ startingKit/js.js
@@ -9,3 +9,15 @@ let phone = 100;
 let tv = 200;
 console.log(calcTax(phone));
 console.log(calcTax(tv));
+
+
+console.log(Math.floor(Math.random()*100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
+
+//string interoplation
+console.log(`I own a pet ${myPet}.`)
+let myName = 'Sylwia';
+let myCity = 'Dublin';
+console.log(`My name is ${myName}. My favorite city is ${myiCty}.`);
