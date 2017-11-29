873575967415889ba7b9365c60c269f3e5c0b43e
sylwia calka <calka.sylwia@gmail.com>
2017-11-29 16:02:38 +0000

    imgur - not done yet

--- requestsIMGUR/script.js
+++ requestsIMGUR/script.js
@@ -2,41 +2,27 @@
 // ----------------------
 // Promises using fetch()
 // ----------------------
-const url = 'https://api.nasa.gov/planetary/apod?api_key=j1ZSJYMZJrrt9LDEi67LUK04zCM3FOox2UTJXWit';
-const url2 = 'https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=j1ZSJYMZJrrt9LDEi67LUK04zCM3FOox2UTJXWit'
+const url = 'https://api.imgur.com/3/album/g99XF -H "Authorization: Client-Id 27b1fcd80f4f0ec"';
+
 let h1 = document.getElementsByTagName('h1')[0];
 let h2 = document.getElementsByTagName('h2')[0];
 let img = document.getElementsByTagName('img')[0];
-
-let heading = document.getElementById('1');
-let secheading = document.getElementById('2');
-let image = document.getElementById('3');
-
-fetch(url)
+//fetch(url, {headerObject})
+fetch(url,  "27b1fcd80f4f0ec")
   .then(function(response) {
-    if(response.ok) return response.json();
+    if(response.ok) return response;
     throw new Error('Request failed.');
   })
   .then(function(data) {
     console.log('fetch() success.',data);
-    h1.innerHTML = data.title;
-    h2.innerHTML = data.copyright;
-    img.src = data.url;
-
+  //  h1.innerHTML = data.title;
+  //  h2.innerHTML = data.copyright;
+  //  img.src = data.url;
+//
   })
   .catch(function(error) {
     console.log(error);
   });
-
-
-fetch(url2)
-  .then(function(response) {
-    if (response.ok) return response.json();
-    throw new Error ('Request failed.');
-  })
-  .then(function(data) {
-    console.log(data);
-  })
 
 
 console.log('fetch() request sent');
