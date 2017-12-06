13cb6cfafe613a53755eeb9e184a9b65a3cfc7c0
sylwia calka <calka.sylwia@gmail.com>
2017-12-06 14:09:25 +0000

    imgur API image fetch based on query

--- requestsIMGUR/script.js
+++ requestsIMGUR/script.js
@@ -2,30 +2,38 @@
 // ----------------------
 // Promises using fetch()
 // ----------------------
-const url = 'https://api.imgur.com/3/album/g99XF -H "Authorization: Client-Id 27b1fcd80f4f0ec"';
+const clientId = '29d360fc53f78ac';
 
 let h1 = document.getElementsByTagName('h1')[0];
 let h2 = document.getElementsByTagName('h2')[0];
 let img = document.getElementsByTagName('img')[0];
-//fetch(url, {headerObject})
-fetch(url,  "27b1fcd80f4f0ec")
-  .then(function(response) {
-    if(response.ok) return response;
-    throw new Error('Request failed.');
-  })
-  .then(function(data) {
-    console.log('fetch() success.',data);
-  //  h1.innerHTML = data.title;
-  //  h2.innerHTML = data.copyright;
-  //  img.src = data.url;
-//
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
 
+const sendRequest = function(){
+  let query = document.getElementById('query').value;
+  const url = `https://api.imgur.com/3/gallery/search/?q=${query}`;
+
+  //fetch(url, {headerObject})
+  fetch(url,  {headers: {'authorization': `Client-ID ${clientId}`}})
+    .then(function(response) {
+      if(response.ok) return response.json();
+      throw new Error('Request failed.');
+    })
+    .then(function(data) {
+
+      data = data.data[0];
+      console.log('fetch() success.',data);
+      h1.innerHTML = data.title;
+      img.src = data.images[0].link;
+  //
+    })
+    .catch(function(error) {
+      console.log('ERROR ' + error);
+    });
+}
 
 console.log('fetch() request sent');
-
+document.getElementById("submit").addEventListener("click", function(){
+    sendRequest();
+});
 
 // Blade Runner 2049 was directed by Denis Villeneuve. It stars: Harrison Ford,RyanGosling,Anade Armas. Its budget was $150,000,000 (estimated).
